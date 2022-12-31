d2134a2210ef8cad6fe27471913238e65a8ee0dc
kalkidan-hub <kalkidandereje666@gmail.com>
2022-12-31 11:55:19 +0300

    Study js

--- Started.js.js
+++ Started.js.js
@@ -1,3 +1,6 @@
-function first(){
-    return 0
-}
+// l=`wanna try a "thing"`
+// l+=`and that is ${2+4}`
+// console.log(l)
+
+conc=`${2+2.0}`+`  ${`${45+90}`}`
+console.log(conc)  //no concatenation of the content, when it's not a stringo
