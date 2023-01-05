0bd24ab43e920307118136eec574602d788c0d44
kalkidan-hub <kalkidandereje666@gmail.com>
2023-01-05 11:23:59 +0300

    Js, Multiplication By Shift

--- Started.js.js
+++ Started.js.js
@@ -1,3 +1,14 @@
-function first(){
-    return 0
+function multiplyByShift(n1,n2){
+    numShift=n2.toString(2)
+    ct=numShift.length-1
+    mul=0
+    for(i in numShift){
+        if(i==1){
+            mul+=n1<<2**ct
+        }
+        ct-=1
+    }
+
+    return mul 
+    // therefore this code does multiplication by shift operation. coooollll
 }
